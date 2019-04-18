452e433c1b3b719b2653e658c25af18db0d0c452
Jessica Hong <jessicahong.24@gmail.com>
2019-04-18 21:56:30 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,3 +3,17 @@ def my_select(collection)
  nums.select {|num| num.even?} 
  
  end
+
+    
+def my_select(array) 
+count = 0
+result = []
+
+while count < array.size
+  if yield(array[count]) == true
+  result.push(array[count])
+end
+  count+=1
+end
+result
+end
